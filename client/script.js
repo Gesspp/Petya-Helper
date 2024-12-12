8c73e7400af31b26bff82cbeb728ed71e940d807
Gesspp <nikitatum199@gmail.com>
2024-12-12 19:29:25 +0300

    закрытие настроек/окна добавление программ

--- client/script.js
+++ client/script.js
@@ -4,15 +4,21 @@ btn.addEventListener('click', () => eel.run_assistant());
 const main = document.querySelector('main');
 const settings = document.querySelector('.settings');
 const add_program = document.querySelector('.add-program');
+const add_program_modal = document.querySelector('.add-program-modal');
 const settings_btn = document.querySelector('.settings-btn');
+const close_modal_button = document.querySelector('.close-modal-button');
 
 async function get_settings() {
     let settings = await eel.get_settings()();
     return settings
 } 
 
-add_program.getEventListener('click', () => {
+add_program.addEventListener('click', () => {
+    add_program_modal.classList.remove('hidden');
+})
 
+close_modal_button.addEventListener('click', () => {
+    add_program_modal.classList.add('hidden');
 })
 const svgFolderIcon = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path d="M496,108.132H273.456l-72.584-71.256H0v87.256v35.488v299.496c0,8.808,7.2,16.008,16,16.008h480c8.8,0,16-7.2,16-16 V124.132C512,115.332,504.8,108.132,496,108.132z M32,68.868h155.792l39.984,39.256H32V68.868z M480,443.124H32V159.62v-19.488 h448V443.124z"/></g></g></svg>`;
 
